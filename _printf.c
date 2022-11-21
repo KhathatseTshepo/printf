388cc92a1f877abe02a19f2f2de9e1397ae13638
Prisoner218070144 <Qrikhotso@yahoo.com>
2022-11-21 20:02:53 +0200

    printf function

--- _printf.c
+++ _printf.c
@@ -6,38 +6,17 @@
  */
 int _printf(const char *format, ...)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
-
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
+	va_list args;
+	int length = 0;
+	
+	if (format == NULL)
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
-	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
-	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	
+	va_start(args, format);
+	
+	va_start(args, format);
+	
+	length = _print_format(formart, args);
+	va_end(args);
+	return (length);
 }
